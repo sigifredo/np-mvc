413a73d61006bf0c3f02975f7e998fba4a58ecfa
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2016-06-26 16:51:23 -0500

    Seleccionamos el controlador y la acción.

--- public/index.php
+++ public/index.php
@@ -4,3 +4,32 @@
 header('Content-Type: text/plain');
 
 chdir(dirname(__DIR__));
+
+if ($_GET['controller'])
+    $controller .= ucwords(strtolower($_GET['controller'])).'Controller';
+else
+    $controller .= 'IndexController';
+
+if ($_GET['action'])
+    $action = strtolower($_GET['action']).'Action';
+else
+    $action = 'indexAction';
+
+$controllerPath = 'src/Controller/'.$controller.'.php';
+
+if (is_file($controllerPath))
+{
+    require $controllerPath;
+
+    if (is_callable(array($controller, $action)))
+    {
+        echo 'existe';
+    }
+    else
+    {
+        // trigger_error ($controller.'->'.$action.' no existe', E_USER_NOTICE);
+        die ('La acción "'.$action.'" no existe - 404 Not found.');
+    }
+}
+else
+    die ('El controlador "'.$controller.'" no existe - 404 Not found.');
