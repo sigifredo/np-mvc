3bbb2c41089a6f7837484d2328bdbee318b56a39
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2016-07-03 14:28:38 -0500

    Reducimos código.

--- lib/NullPoint/Core/Init.php
+++ lib/NullPoint/Core/Init.php
@@ -3,9 +3,7 @@
 function getRequest()
 {
     if (isset($_GET['company']) && !isset($_GET['controller']))
-    {
         return (object) array ('controller' => 'CompanyController', 'action' => 'profileAction');
-    }
     else if (!isset($_GET['company']))
     {
         if (isset($_GET['controller']))
